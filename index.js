3263e2af9a67a323708db01bdfd06692621e8d0f
mohammadsh96 <mhmd.shrydh1996@gmail.com>
2022-06-28 01:30:15 +0300

    html and js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port =4000 ; 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  
+  io.on('connection', (socket) => {
+    console.log("user connected");
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+
+server.listen(port, () => {
+  console.log(`listening on : ${port}`);
+});
